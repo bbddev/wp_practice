9f2c29ce0ddc10f487cf92133ea69d27e1392dca
BinhVo <bbluvcode@gmail.com>
2025-09-24 16:26:42 +0700

    Enhance CRUD plugin: add update functionality and edit form handling

--- wp-content/plugins/my-crud-plugin/my-crud-plugin.php
+++ wp-content/plugins/my-crud-plugin/my-crud-plugin.php
@@ -34,8 +34,8 @@ function my_crud_plugin_admin_page()
 {
     global $wpdb;
     //for delete code
+    $table_name = $wpdb->prefix . 'my_crud_data';
     if (isset($_GET['action']) && $_GET['action'] === 'delete' && isset($_GET['id'])) {
-        $table_name = $wpdb->prefix . 'my_crud_data';
         $id = intval($_GET['id']);
         $deleted = $wpdb->delete($table_name, ['id' => $id]);
         if ($deleted) {
@@ -48,7 +48,6 @@ function my_crud_plugin_admin_page()
     if (isset($_POST['my_crud_submit'])) {
         $name = sanitize_text_field($_POST['name'] ?? '');
         $email = sanitize_email($_POST['email'] ?? '');
-        $table_name = $wpdb->prefix . 'my_crud_data';
         $wpdb->insert(
             $table_name,
             [
@@ -58,11 +57,45 @@ function my_crud_plugin_admin_page()
         );
         echo '<div class="updated"><p>Data added successfully!</p></div>';
     }
+
+    // Handle update submission
+    if (isset($_POST['my_crud_update'])) {
+        $id = intval($_POST['id']);
+        $name = sanitize_text_field($_POST['name']);
+        $email = sanitize_email($_POST['email']);
+
+        $updated = $wpdb->update(
+            $table_name,
+            ['name' => $name, 'email' => $email],
+            ['id' => $id]
+        );
+        if ($updated !== false) {
+            echo '<div class="updated"><p>Record updated successfully!</p></div>';
+        } else {
+            echo '<div class="error"><p>Error updating record.</p></div>';
+        }
+    }
+
+    //Check if editing
+    $editing = false;
+    $edit_id = 0;
+    $edit_name = '';
+    $edit_email = '';
+
+    if (isset($_GET['action']) && $_GET['action'] === 'edit' && isset($_GET['id'])) {
+        $edit_id = intval($_GET['id']);
+        $row = $wpdb->get_row("SELECT * FROM $table_name WHERE id = $edit_id");
+        if ($row) {
+            $editing = true;
+            $edit_name = esc_attr($row->name);
+            $edit_email = esc_attr($row->email);
+        }
+    }
+
     ?>
-   
 
     <div class="wrap">
-        <h1>My CRUD Plugin</h1>
+        <h1><?php echo $editing ? 'Edit Record' : 'My CRUD Plugin'; ?></h1>
         <form action="" method="post">
             <table class="form-data">
                 <tr>
@@ -70,7 +103,7 @@ function my_crud_plugin_admin_page()
                         <label for="name">Name</label>
                     </th>
                     <td>
-                        <input type="text" id="name" name="name" required>
+                        <input type="text" id="name" name="name" required value="<?php echo $edit_name; ?>">
                     </td>
                 </tr>
                 <tr>
@@ -78,13 +111,18 @@ function my_crud_plugin_admin_page()
                         <label for="email">Email</label>
                     </th>
                     <td>
-                        <input type="email" id="email" name="email" required>
+                        <input type="email" id="email" name="email" required value="<?php echo $edit_email; ?>">
                     </td>
                 </tr>
                 <tr>
-                    <th></th>
                     <td>
-                        <input type="submit" name="my_crud_submit" value="Add Record" class="button button-primary">
+                        <?php if ($editing): ?>
+                            <input type="hidden" name="id" value="<?php echo $edit_id; ?>">
+                            <input type="submit" name="my_crud_update" value="Update Record" class="button button-primary">
+                            <a href="?page=my-crud-plugin" class="button">Cancel</a>
+                        <?php else: ?>
+                            <input type="submit" name="my_crud_submit" value="Add Record" class="button button-primary">
+                        <?php endif; ?>
                     </td>
                 </tr>
             </table>
