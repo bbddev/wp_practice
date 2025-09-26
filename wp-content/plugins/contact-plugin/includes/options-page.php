9596daf65eb7e0cfe397b299050e58b899408d40
BinhVo <bbluvcode@gmail.com>
2025-09-26 13:38:37 +0700

    2:24:24 Sort Plugin Menu Order

--- wp-content/plugins/contact-plugin/includes/options-page.php
+++ wp-content/plugins/contact-plugin/includes/options-page.php
@@ -19,15 +19,16 @@ function load_carbon_fields()
 function create_options_page()
 {
     Container::make('theme_options', __('Contact Form'))
+        ->set_page_menu_position(30)
         ->set_icon('dashicons-media-text')
         ->add_fields(array(
 
             Field::make('checkbox', 'contact_plugin_active', __('Active')),
-            
+
             Field::make('text', 'contact_plugin_recipients', __('Recipients Emails'))
                 ->set_attribute('placeholder', 'eg. email@example.com')
                 ->set_help_text('The email that the form is submitted to.'),
-                
+
             Field::make('textarea', 'contact_plugin_message', __('Confirmation Message'))
                 ->set_attribute('placeholder', 'Enter your message here...')
                 ->set_help_text('Type the message you want the submitter to receive'),
